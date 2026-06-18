bc4e0b350181292aa4b0d06ac754ea4412d8a5ff
Fadila Yiddana <dilayidd10@gmail.com>
2026-06-18 23:46:40 +0000

    Add DynamoDB table resource for visitor counter

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
 resource "aws_s3_bucket" "website_bucket" {
   bucket = var.bucket_name
 }
+
+resource "aws_dynamodb_table" "visitor_counter" {
+  name         = "visitor-counter"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
